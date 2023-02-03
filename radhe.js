427ce9c184db46d7e193106cc6f8a3ce45fa8194
Radha743 <worldofnara777@gmail.com>
2023-02-04 00:34:09 +0530

    Adding and deletion

--- radhe.js
+++ radhe.js
@@ -1,25 +1,91 @@
 console.log("Radhey");
 
+const dummyTodos = [
+	{ id: "4555454865463", task: "Chant Radhey Krsna", isCompleted: false },
+	{ id: "4234242342342", task: "Paint some picture", isCompleted: true },
+	{ id: "2342342342342", task: "Code some website", isCompleted: false },
+];
+
+class Todo {
+	constructor(task) {
+		this.id = (Math.floor(Math.random() * 1000) + Date.now()).toString();
+		this.task = task;
+		this.isCompleted = false;
+	}
+}
+
+/**
+ * @type {Todo[]} todos
+ */
+
+let todos = [...dummyTodos];
+
 const todoHolder = $("[data-todo-holder]");
 
 /**
  * @type {HTMLInputElement} todoInput
  */
 const todoInput = $("[data-todo-input]");
+todoInput.addEventListener("keydown", handleTodoInput);
 
-todoInput.addEventListener("keydown", (e) => {
+// Functions
+function handleTodoInput(e) {
 	if (e.key === "Enter") {
 		if (e.target.value === "") return;
-		const newTodo = document.createElement("div");
-		newTodo.classList.add("todo");
-		newTodo.setAttribute("data-todo", "");
-		newTodo.innerHTML = `
-    	<input type="checkbox" name="" id=""/>
-						<p>${e.target.value}</p>`;
-		todoHolder.appendChild(newTodo);
+
+		const newTodo = new Todo(e.target.value);
+		const newTodoDiv = createTodoDiv(newTodo);
+
+		todos.push(newTodo);
+
+		todoHolder.appendChild(newTodoDiv);
 		e.target.value = "";
+		console.log(todos);
 	}
-});
+}
+
+function createTodoDiv({ task, id, isCompleted }) {
+	const newTodoDiv = document.createElement("div");
+	newTodoDiv.classList.add("todo");
+	newTodoDiv.setAttribute("data-todo", "");
+	newTodoDiv.setAttribute("data-todo-id", id);
+
+	const newInput = document.createElement("input");
+	newInput.type = "checkbox";
+	newInput.checked = isCompleted;
+
+	const newP = document.createElement("p");
+	newP.appendChild(document.createTextNode(task));
+
+	const span = document.createElement("span");
+	span.setAttribute("data-delete-todo", "");
+
+	const img = document.createElement("img");
+	img.src = "./assets/images/icon-cross.svg";
+
+	span.appendChild(img);
+
+	[newInput, newP, span].forEach((item) => newTodoDiv.appendChild(item));
+
+	newTodoDiv
+		.querySelector("span")
+		.addEventListener("click", handleTodoDeletion);
+	return newTodoDiv;
+}
+
+function handleTodoDeletion(e) {
+	/**
+	 * @type {HTMLDivElement} parenTodoDiv
+	 */
+	const parentTodoDiv = e.target.parentElement.parentElement;
+
+	todos = todos.filter(
+		(todo) => todo.id !== parentTodoDiv.getAttribute("data-todo-id")
+	);
+	console.log(todos);
+
+	parentTodoDiv.remove();
+}
 
 /**
  *
@@ -39,3 +105,9 @@ function $$(selector) {
 function $(selector) {
 	return document.querySelector(selector);
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+	todos.forEach((todo) => {
+		todoHolder.appendChild(createTodoDiv(todo));
+	});
+});
