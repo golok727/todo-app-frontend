fdfcc60d485d0334802380886a8e7aada744b0fc
Radha743 <worldofnara777@gmail.com>
2023-02-03 23:31:50 +0530

    Darkmode

--- radhe.js
+++ radhe.js
@@ -1,1 +1,41 @@
 console.log("Radhey");
+
+const todoHolder = $("[data-todo-holder]");
+
+/**
+ * @type {HTMLInputElement} todoInput
+ */
+const todoInput = $("[data-todo-input]");
+
+todoInput.addEventListener("keydown", (e) => {
+	if (e.key === "Enter") {
+		if (e.target.value === "") return;
+		const newTodo = document.createElement("div");
+		newTodo.classList.add("todo");
+		newTodo.setAttribute("data-todo", "");
+		newTodo.innerHTML = `
+    	<input type="checkbox" name="" id=""/>
+						<p>${e.target.value}</p>`;
+		todoHolder.appendChild(newTodo);
+		e.target.value = "";
+	}
+});
+
+/**
+ *
+ * @param {string} selector
+ * @returns {Element[] | null}
+ */
+
+function $$(selector) {
+	return [...document.querySelectorAll(selector)];
+}
+
+/**
+ *
+ * @param {string} selector
+ * @returns {Element | null}
+ */
+function $(selector) {
+	return document.querySelector(selector);
+}
